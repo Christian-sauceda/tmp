76eaf8e699ee0f8ed48fff0ef90f3e66b91452f1
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2023-06-19 22:02:35 -0600

    Update Skeletor.jsx

--- front/src/components/Alerts/Skeletor.jsx
+++ front/src/components/Alerts/Skeletor.jsx
@@ -1,105 +1,97 @@
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
-// components
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function CardFileAddMovieAdult() {
-    return (
-        <>
-            <div className="w-full flex flex-col sm:flex-row flex-grow overflow-hidden">
-                <div className="sm:w-1/3 md:1/4 w-full flex-shrink flex-grow-0 p-4">
-                    <div className="sticky top-0 p-4 w-full">
-                        <ul className="flex sm:flex-col overflow-hidden content-center ">
-                            <Skeleton direction="ltr" width="300px" height="900px" borderRadius="35px" />
-                        </ul>
-                    </div>
-                </div>
-                <main role="main" className="w-auto h-auto flex-grow pt-10 ">
+  const generateRandomHeight = () => {
+    return Math.floor(Math.random() * (20 - 10 + 1) + 10); // Genera una altura aleatoria entre 100 y 200
+  };
 
-                    <p className="mb-0 borde"> <Skeleton direction="ltr" width="1500px" height="150px" borderRadius="25px" /></p>
-                    <div className=" p-8 grid grid-rows-2 grid-flow-col gap-3 auto-cols-fr">
+  const generateRandomWidth = () => {
+    return Math.floor(Math.random() * (20 - 10 + 1) + 100); // Genera un ancho aleatorio entre 100 y 200
+  };
 
-                        <div className="p-4 bg-slate-100 flex gap-2">
-                            <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                            <div className='align-middle'>
-                                <Skeleton direction="ltr" width="200px" height="85px" borderRadius="10px" />
-                                <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                            </div>
-                        </div>
-                        <div className="p-4 bg-slate-100 flex gap-2">
-                            <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                            <div className='align-middle'>
-                                <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                                <Skeleton direction="ltr" width="170px" height="45px" borderRadius="10px" />
-                            </div>
-                        </div>
-                        <div className="p-4 bg-slate-100 flex gap-2 ">
-                            <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                            <div className='align-middle'>
-                                <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                                <Skeleton direction="ltr" width="100px" height="45px" borderRadius="10px" />
-                            </div>
-                        </div>
-                        <div className="p-4 bg-slate-100 flex gap-2 ">
-                            <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                            <div className='align-middle'>
-                                <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                                <Skeleton direction="ltr" width="200px" height="85px" borderRadius="10px" />
-                            </div>
-                        </div>
-                        <div className="p-4 bg-slate-100 flex gap-2 ">
-                            <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                            <div className='align-middle'>
-                                <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                                <Skeleton direction="ltr" width="150px" height="45px" borderRadius="px" />
-                            </div>
-                        </div>
-                        <div className="p-4 bg-slate-100 flex gap-2 ">
-                            <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                            <div className='align-middle'>
-                                <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                                <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                            </div>
-                        </div>
-                        <div className="p-4 bg-slate-100 flex gap-2">
-                            <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                            <div className='align-middle'>
-                                <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                                <Skeleton direction="ltr" width="200px" height="85px" borderRadius="10px" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="p-3 bg-slate-100 flex gap-1">
-                        <div className='align-middle flex gap-4'>
-                            <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                            <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                            <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                            <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                            <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                            <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                        </div>
-                    </div>
-                    <div className='grid grid-cols-2'>
-                        <div className=" p-8 flex gap-2">
+  const renderRandomBoxes = () => {
+    const boxCount = 6; // Cantidad de cuadros aleatorios
+    const boxes = [];
 
-                            <div className="p-4 bg-slate-100 flex gap-2 ">
-                                <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                                <div className='align-middle'>
-                                    <Skeleton direction="ltr" width="300px" height="45px" borderRadius="10px" />
-                                    <Skeleton direction="ltr" width="200px" height="45px" borderRadius="10px" />
-                                </div>
-                            </div>
-                            <div className="p-4 bg-slate-100 flex gap-2">
-                                <p className="mb-4"> <Skeleton direction="ltr" width="100px" height="100px" borderRadius="100px" /></p>
-                                <div className='align-middle'>
-                                    <Skeleton direction="ltr" width="400px" height="95px" borderRadius="10px" />
-                                    <Skeleton direction="ltr" width="170px" height="45px" borderRadius="10px" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
-                <p className="mb-0"> <Skeleton direction="ltr" width="1500px" height="200px" baseColor='000' /></p>
+    for (let i = 0; i < boxCount; i++) {
+      const height = generateRandomHeight();
+      const width = generateRandomWidth();
+      const key = `box-${i}`;
+
+      boxes.push(
+        <div className="bg-white rounded-md shadow p-4 mb-4" key={key}>
+          <div className="w-full sm:w-auto h-32 bg-gray-200 rounded-md mb-4">
+            <Skeleton height={32} width="100%" direction="ltr" />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className={` w-full sm:w-auto w-${width} h-${height} bg-gray-200 rounded-md`}>
+              <Skeleton height={height} width="100%" direction="ltr" />
             </div>
-        </>
-    );
+            <div className={`w-full sm:w-auto w-${width} h-${height} bg-gray-200 rounded-md`}>
+              <Skeleton height={20} width="100%" direction="ltr" />
+            </div>
+            <div className={`w-full sm:w-auto w-${width} h-${height} bg-gray-200 rounded-md`}>
+              <Skeleton height={height} width="100%" direction="ltr" />
+            </div>
+            <div className={`w-full sm:w-auto w-${width} h-${height} bg-gray-200 rounded-md`}>
+              <Skeleton height={height} width="100%" direction="ltr" />
+            </div>
+            <div className={ `w-full sm:w-auto w-${width} h-${height} bg-gray-200 rounded-md`}>
+              <Skeleton height={15} width="100%" direction="ltr" />
+            </div>
+            <div className={`w-full sm:w-auto w-${width} h-${height} bg-gray-200 rounded-md`}>
+              <Skeleton height={10} width="100%" direction="ltr" />
+            </div>
+            
+          </div>
+        </div>
+      );
+    }
+
+    return boxes;
+  };
+
+  return (
+    <div className="bg-gray-100 rounded-md shadow-lg p-4">
+      <div className="flex items-center justify-between mb-4">
+
+      </div>
+
+      <div className="flex">
+        <div className="w-5/5 bg-gray-300 rounded-md p-4 mr-4">
+          <ul className="space-y-4">
+            <li className="cursor-pointer text-blue-500 font-medium hover:text-blue-600 transition-colors duration-300 relative">
+              <Skeleton width={250} direction="ltr" />
+              <span className="absolute h-1 bg-blue-500 left-0 bottom-0 w-full transform scale-x-0 transition-transform duration-300"></span>
+            </li>
+            <li className="cursor-pointer text-blue-500 font-medium hover:text-blue-600 transition-colors duration-300 relative">
+              <Skeleton width={80} direction="ltr" />
+              <span className="absolute h-1 bg-blue-500 left-0 bottom-0 w-full transform scale-x-0 transition-transform duration-300"></span>
+            </li>
+            <li className="cursor-pointer text-blue-500 font-medium hover:text-blue-600 transition-colors duration-300 relative">
+              <Skeleton width={250} direction="ltr" />
+              <span className="absolute h-1 bg-blue-500 left-0 bottom-0 w-full transform scale-x-0 transition-transform duration-300"></span>
+            </li>
+            <li className="cursor-pointer text-blue-500 font-medium hover:text-blue-600 transition-colors duration-300 relative">
+              <Skeleton width={80} direction="ltr" />
+              <span className="absolute h-1 bg-blue-500 left-0 bottom-0 w-full transform scale-x-0 transition-transform duration-300"></span>
+            </li>
+            <li className="cursor-pointer text-blue-500 font-medium hover:text-blue-600 transition-colors duration-300 relative">
+              <Skeleton width={250} direction="ltr" />
+              <span className="absolute h-1 bg-blue-500 left-0 bottom-0 w-full transform scale-x-0 transition-transform duration-300"></span>
+            </li>
+           
+          </ul>
+        </div>
+
+        <div className="w-4/5">
+          <div className="grid grid-cols-3 gap-4">
+            {renderRandomBoxes()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
