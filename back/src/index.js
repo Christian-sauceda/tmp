0b11a97483d6d7aa407f632996803756e63665fa
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2023-06-23 10:02:23 -0600

    resolver problemas de tiempo de inactividad

--- back/src/index.js
+++ back/src/index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 //const { startCronJobSemanal } = require("./cronServiceReportSemanal");
 const { startCronJobDiario } = require("./cronServiceReportDiario");
 
+// Configurar tiempo de espera para las solicitudes (en milisegundos)
+const timeoutInMs = 0; // Deshabilitar el tiempo de espera (0) o establecer un valor específico en milisegundos
+app.timeout = timeoutInMs;
 
 let port = process.env.PORT;
 app.listen(port);
