019e10cc749eb44e343234b92229767bf365d7e1
Christian-sauceda <81939528+Christian-sauceda@users.noreply.github.com>
2024-03-06 22:00:17 -0600

    Update Login.jsx

--- Front-V2/src/pages/Auth/Login.jsx
+++ Front-V2/src/pages/Auth/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
 >
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-5 w-5 text-gray-400"
+    className="h-5 w-5 text-gray-400"
     viewBox="0 0 24 24"
     fill="currentColor"
   >
